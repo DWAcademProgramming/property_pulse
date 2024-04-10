ae50051b8c89612923892272009b90e0a1210eac
David <95488893+DWAcademProgramming@users.noreply.github.com>
2024-04-10 09:47:40 -0400

    Add files via upload

---
+++ app/layout.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import '@/assets/styles/globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; 
+
+export const metadata ={ 
+    title: 'PropertyPulse | Find the Perfect Rental', 
+    description: 'Find your dream rental property', 
+    keywords: 'rental, find rentals'
+}; 
+
+const MainLayout  = ({children}) => {
+  return (
+    <AuthProvider>
+      <html lang='en'>
+          <body>
+              <Navbar />
+              <main>
+                  {children}
+              </main>
+              <Footer />
+              <ToastContainer /> 
+          </body>
+      </html>
+    </AuthProvider>
+  )
+}
+
+export default MainLayout; 
---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hero from '@/components/Hero';
+import InfoBoxes from '@/components/InfoBoxes';
+import HomeInfoProperties from '@/components/HomeInfoProperties';
+
+const HomePage = () => {
+  return (
+    <>
+        <Hero />
+        <InfoBoxes /> 
+        <HomeInfoProperties />
+    </>
+  )
+}
+
+export default HomePage
